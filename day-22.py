3625c9913c234552885cba52f2cfecb9d51883fb
Andrés ML <andres.mingorance@gmail.com>
2019-12-25 10:07:20 +0100

    Day 22 part 2

--- day-22.py
+++ day-22.py
@@ -1,7 +1,6 @@
 from utils import cmap, compose
 from text import toLines
 from functools import partial, reduce
-import math
 
 def parse_move(line):
     if line == 'deal into new stack':
@@ -22,9 +21,23 @@ reverse = lambda index, deck_size: deck_size - index - 1
 apply_move = lambda index, deck_size, move, args: globals()[move](*args, index, deck_size)
 shuffle_index = lambda index, deck_size, shuffle: reduce(lambda index, move: apply_move(index, deck_size, move['move'], move['args']), shuffle, index)
 
-# reverse_offset = lambda value, *args: offset(-value, *args)
-# reverse_cut = lambda value, *args: cut(-value, *args)
-# reverse_reverse = reverse
-# reverse_shuffle = lambda shuffle: [{**move, 'move': 'reverse_' + move['move']} for move in reversed(shuffle)]
+# not my solution
+def find(times, final_position, deck_size, moves):
+    offset = lambda value: (value, 0)
+    cut = lambda value: (1, -value)
+    reverse = lambda : (-1, -1)
+    
+    a, b = 1, 0
+    for move in moves:
+        la, lb = locals()[move['move']](*move['args'])
+        a = (la * a) % deck_size
+        b = (la * b + lb) % deck_size
+
+    modinv = lambda a, n: pow(a, n - 2, n)
+
+    Ma = pow(a, times, deck_size)
+    Mb = (b * (Ma - 1) * modinv(a-1, deck_size)) % deck_size
+    return ((final_position - Mb) * modinv(Ma, deck_size)) % deck_size
     
 one = compose(partial(shuffle_index, 2019, 10007), parse)
+two = compose(partial(find, 101741582076661, 2020, 119315717514047), parse)
